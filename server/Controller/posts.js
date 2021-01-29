b878269656b8bc9addbd272f8180f7206cc2c86e
sg60692 <61661622+sg60692@users.noreply.github.com>
2021-01-29 12:34:51 +0530

    sarthak

--- server/Controller/posts.js
+++ server/Controller/posts.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const question = require('../models/question.js');
-
+const users = require('../models/user.js');
 const getPosts = (req, res) => {
     res.send("Hello World!");
 }
@@ -10,9 +10,10 @@ const postQuestion = async (req, res) => {
     const newQuestion = new question(data);
     try {
         await newQuestion.save();
-        res.status(200).json(newQuestion);
+        await users.findByIdAndUpdate(newQuestion.userId, { $push: { questions: newQuestion._id } });
+        res.status(200).json(1);
     } catch (error) {
-        res.status(404).json({ message: error });
+        res.status(404).json(2);
     }
 }
 
