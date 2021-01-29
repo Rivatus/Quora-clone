25dc9b49765db0cbdffe3a0436fb023ff64a6387
sg60692 <61661622+sg60692@users.noreply.github.com>
2021-01-29 11:21:39 +0530

    sarthak-mongo cluster

--- server/index.js
+++ server/index.js
@@ -12,11 +12,10 @@ app.use("/api", route);
 
 const PORT = process.env.PORT || 5000;
 
-const CONNECTION_URL = "mongodb+srv://Rivatus:tH2fSILAEOJBNyPb@cluster0.2s51y.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://sarthak:<password>@cluster0.u4xph.mongodb.net/<dbname>?retryWrites=true&w=majority";
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server started on port ${PORT}`)))
     .catch((error) => console.log(error));
 
 mongoose.set('useFindAndModify', false);
-console.log("hi");
