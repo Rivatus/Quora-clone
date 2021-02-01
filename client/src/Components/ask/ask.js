93eb7be775f95f581dd27476da9b7182c62d8680
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-01 19:50:23 +0530

    sarthak

--- client/src/Components/ask/ask.js
+++ client/src/Components/ask/ask.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { askQuestion } from '../../actions/index.js';
-
+import './ask.css';
 const AskForm = () => {
 
     const dispatch = useDispatch();
@@ -11,21 +11,23 @@ const AskForm = () => {
         dispatch(askQuestion());
     }
 
-    return <form onSubmit={handleSubmit}>
-        <div class="form-group">
-            <label>Question</label>
-            <textarea class="form-control" rows="3" placeholder="Enter your question here..." ></textarea>
-        </div>
-        <div class="form-group">
-            <label>Description</label>
-            <textarea class="form-control" rows="5" placeholder="Give description about your question" ></textarea>
-        </div>
-        <div class="form-group">
-            <label>Tags</label>
-            <textarea class="form-control" rows="2" placeholder="Add tags to your question" ></textarea>
-        </div>
-        <button type="button" class="btn btn-danger">Ask</button>
-    </form>
+    return  <div className="Askform">
+                <form onSubmit={handleSubmit}>
+                    <div class="form-group">
+                        <label>Question</label>
+                        <textarea class="form-control" rows="3" placeholder="Enter your question here..." ></textarea>
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea class="form-control" rows="5" placeholder="Give description about your question" ></textarea>
+                </div>
+                <div class="form-group">
+                    <label>Tags</label>
+                    <textarea class="form-control" rows="2" placeholder="Add tags to your question" ></textarea>
+                </div>
+                <button type="button" class="btn btn-danger">Ask</button>
+                </form>
+            </div>
 }
 
 export default AskForm;
