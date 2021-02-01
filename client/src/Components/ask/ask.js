37a843f44bf6a0dbb604cffd2927ffb909e66977
rivatus <>
2021-02-01 20:21:48 +0530

    Update ask.js

--- client/src/Components/ask/ask.js
+++ client/src/Components/ask/ask.js
@@ -13,9 +13,9 @@ const AskForm = () => {
 
     return <div className="Askform">
         <form onSubmit={handleSubmit} >
-            <div className="form-group was-validated" >
+            <div className="form-group" >
                 <label>Question</label>
-                <textarea className="form-control is-invalid" rows="3" placeholder="Enter your question here..." required></textarea>
+                <textarea className="form-control" rows="3" placeholder="Enter your question here..." required></textarea>
             </div>
             <div className="form-group">
                 <label>Description</label>
