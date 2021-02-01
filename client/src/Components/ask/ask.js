356404d970c9f4978626cf6cf54c4715a310b913
rivatus <>
2021-02-01 19:42:40 +0530

    arpit

--- client/src/Components/ask/ask.js
+++ client/src/Components/ask/ask.js
@@ -11,11 +11,20 @@ const AskForm = () => {
         dispatch(askQuestion());
     }
 
-    return <form>
+    return <form onSubmit={handleSubmit}>
         <div class="form-group">
             <label>Question</label>
-            <textarea class="form-control" rows="3"></textarea>
+            <textarea class="form-control" rows="3" placeholder="Enter your question here..." ></textarea>
         </div>
+        <div class="form-group">
+            <label>Description</label>
+            <textarea class="form-control" rows="5" placeholder="Give description about your question" ></textarea>
+        </div>
+        <div class="form-group">
+            <label>Tags</label>
+            <textarea class="form-control" rows="2" placeholder="Add tags to your question" ></textarea>
+        </div>
+        <button type="button" class="btn btn-danger">Ask</button>
     </form>
 }
 
