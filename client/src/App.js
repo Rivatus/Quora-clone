7c63910ee2387bdf16705d55cc8b16af76ba1f47
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-01 19:44:31 +0530

    sarthak

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import AskForm from './Components/ask/ask.js';
-
+import Navbar from './Components/navbar/navbar.js'
 function App() {
   return (
     <div>
       <header>
-        <h1> Hello World </h1>
+        <Navbar/>
         <AskForm />
       </header>
     </div>
--- client/src/Components/navbar/navbar.js
+++ client/src/Components/navbar/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './navbar.css';
 function dashboard()
 {
 
@@ -17,8 +18,51 @@ function login_logout()
 }
 function navbar() {
     return (
-        <div>
-            
+        <div id="Navbar">
+            <nav className="navbar navbar-expand-lg navbar-dark">
+                <button
+                    className="navbar-toggler btne"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon bt"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto ds">  
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                            <a href="#Subsidiaries" className="nm">
+                               DashBoard
+                            </a> 
+                        </li>  
+                        <li>
+                            <input type="text"
+                                className="px-3 py-1 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
+                                placeholder="Subject" 
+                                label="Subject"
+                            />
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">                     
+                            <a href="#About" className="nm">
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show">
+                            <a href="#Products" className="nm">
+                                Ask?
+                            </a>
+                        </li>  
+                        <li className="nav-item" data-toggle="collapse" data-target=".navbar-collapse.show"> 
+                            <a href="#Testimonials" className="nm">
+                                Login
+                            </a>
+                        </li>                                             
+                    </ul>
+                </div>
+            </nav>
         </div>
     )
 }
