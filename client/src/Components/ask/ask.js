f0f3bb28c223f25286753fa5239717e2af8f217b
rivatus <>
2021-02-01 20:13:29 +0530

    arpit

--- client/src/Components/ask/ask.js
+++ client/src/Components/ask/ask.js
@@ -11,23 +11,23 @@ const AskForm = () => {
         dispatch(askQuestion());
     }
 
-    return  <div className="Askform">
-                <form onSubmit={handleSubmit}>
-                    <div class="form-group">
-                        <label>Question</label>
-                        <textarea class="form-control" rows="3" placeholder="Enter your question here..." ></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Description</label>
-                    <textarea class="form-control" rows="5" placeholder="Give description about your question" ></textarea>
-                </div>
-                <div class="form-group">
-                    <label>Tags</label>
-                    <textarea class="form-control" rows="2" placeholder="Add tags to your question" ></textarea>
-                </div>
-                <button type="button" class="btn btn-danger">Ask</button>
-                </form>
+    return <div className="Askform">
+        <form onSubmit={handleSubmit} >
+            <div className="form-group was-validated" >
+                <label>Question</label>
+                <textarea className="form-control is-invalid" rows="3" placeholder="Enter your question here..." required></textarea>
             </div>
+            <div className="form-group">
+                <label>Description</label>
+                <textarea className="form-control" rows="5" placeholder="Give description about your question" ></textarea>
+            </div>
+            <div className="form-group">
+                <label>Tags</label>
+                <textarea className="form-control" rows="2" placeholder="Add tags to your question" ></textarea>
+            </div>
+            <button type="button" className="btn btn-danger askFormButton" >Ask</button>
+        </form>
+    </div>
 }
 
 export default AskForm;
