b0123c8b9dbb0fcea080870c111408a7ee1b4551
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-03 21:04:18 +0530

    sarthak

--- client/src/reducers/message.js
+++ client/src/reducers/message.js
@@ -1,10 +1,12 @@
 export default (message = { status: 0 }, action) => {
+    console.log("hi");
     console.log(action.type);
+    console.log(action.payload);
     switch (action.type) {
         case "Ask":
             return { status: action.payload };
         case "Failure":
-            return { status: 0 };
+            return { status: 2 };
         default:
             return message;
     }
