2130ddff0ecf53921b363075bb8de51fd5e91dd2
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-03 19:54:59 +0530

    sarthak

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,8 +3,8 @@ import * as api from '../api/index.js';
 export const askQuestion = (question) => async (dispatch) => {
     try {
 
-        await api.askquestion(question);
-        dispatch({ type: "Ask" });
+        const data=await api.askquestion(question);
+        dispatch({ type: "Ask",payload:data});
     } catch (error) {
         console.log(error);
         dispatch({ type: "Failure" });
