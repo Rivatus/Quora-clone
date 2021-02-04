5b98a12e0905cfb0bf2a32483f261ed31e682f12
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-04 14:52:20 +0530

    sarthak

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,10 +3,9 @@ import * as api from '../api/index.js';
 export const askQuestion = (question) => async (dispatch) => {
     try {
         const data = await api.askquestion(question);
-
         await dispatch({ type: "Ask", payload: data.data });
     } catch (error) {
         console.log(error);
-        dispatch({ type: "Failure", payload: 2 });
+        await dispatch({ type: "Ask", payload: 2 });
     }
 }
--- client/src/reducers/message.js
+++ client/src/reducers/message.js
@@ -2,8 +2,8 @@ export default (message = { status: 0 }, action) => {
     switch (action.type) {
         case "Ask":
             return { status: action.payload };
-        case "Failure":
-            return { status: 2 };
+        case "Reset":
+            return { status:0};
         default:
             return message;
     }
