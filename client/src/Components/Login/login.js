425177be09cd5c75df129daf1621f4bdc7dcf620
rivatus <>
2021-02-05 15:41:40 +0530

    arpit

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './login.css';
 
 const Login = () => {
-    return <div style={{marginTop:"200px"}}>HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia</div>
+    return <div class="container mt-10">
+        <h1 style={{ marginTop: "60px" }} >Login</h1>
+
+        <div class="row">
+
+            <div class="col-sm-4">
+                <div class="card social-block">
+                    <div class="card-body">
+                        <a class="btn btn-block btn-google" href="/auth/google" role="button">
+                            <i class="fab fa-google"></i>
+                            &nbsp;Sign Up with Google
+          </a>
+                    </div>
+                </div>
+                <div class="card social-block">
+                    <div class="card-body">
+                        <a class="btn btn-block btn-facebook" href="/auth/facebook" role="button">
+                            <i class="fab fa-facebook"></i>
+            &nbsp;Sign Up with Facebook
+          </a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
 }
 
 export default Login;
