c2a3dc040abb7a54357559b58e373dbe63f015ea
rivatus <>
2021-02-05 16:38:28 +0530

    arpit

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Ask from './Pages/Ask.js';
 import Navbar from './Components/navbar/navbar.js';
-import Login from './Components/Login/login.js';
+import LoginPage from './Pages/Login.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
@@ -10,13 +10,13 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/ask">
-            <Ask/>
+            <Ask />
           </Route>
           <Route path="/login">
-            <Login />
+            <LoginPage />
           </Route>
           <Route path="/">
-            <h1 style={{marginTop:"200px"}}>
+            <h1 style={{ marginTop: "200px" }}>
               Home
               </h1>
           </Route>
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Login from '../Components/Login/login.js';
+
+function LoginPage() {
+    return <div>
+        <Login />
+    </div>
+}
+
+export default LoginPage;
