b56d4811d4b4b9420eebaab8d442ee03568c8ebc
rivatus <>
2021-02-05 15:26:01 +0530

    arpit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
 import AskForm from './Components/ask/ask.js';
 import Navbar from './Components/navbar/navbar.js';
-import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Login from './Components/Login/login.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <div className="App">
-          <Navbar/>
-          <Switch>
-            <Route path="/ask">
-              <AskForm/>
-            </Route>
-            <Route path="/">
-              <h1>
-                Home
+        <Navbar />
+        <Switch>
+          <Route path="/ask">
+            <AskForm />
+          </Route>
+          <Route path="/">
+            <h1>
+              Home
               </h1>
-            </Route>
-          </Switch>
-          
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+
       </div>
     </Router>
   );
---
+++ client/src/Components/Login/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+    return <div>HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia HIkjnakdjn asjndawkia</div>
+}
+
+export default Login;
