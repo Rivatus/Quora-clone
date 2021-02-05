1ef8213caf72227edfec8e8bddb5cf2309f62c05
rivatus <>
2021-02-05 16:00:38 +0530

    arpit

--- client/src/Components/Login/login.js
+++ client/src/Components/Login/login.js
@@ -3,25 +3,25 @@ import './login.css';
 
 const Login = () => {
     return <div class="container mt-10">
-        <h1 style={{ marginTop: "60px" }} >Login</h1>
+        <h1 className="LoginHeading">Login</h1>
 
-        <div class="row">
+        <div className="row">
 
-            <div class="col-sm-4">
-                <div class="card social-block">
-                    <div class="card-body">
-                        <a class="btn btn-block btn-google" href="/auth/google" role="button">
-                            <i class="fab fa-google"></i>
+            <div className="col-sm-4 Authentication">
+                <div className="card social-block">
+                    <div className="card-body">
+                        <a className="btn btn-block btn-google" href="/auth/google" role="button">
+                            <i className="fab fa-google"></i>
                             &nbsp;Sign Up with Google
-          </a>
+                        </a>
                     </div>
                 </div>
-                <div class="card social-block">
-                    <div class="card-body">
-                        <a class="btn btn-block btn-facebook" href="/auth/facebook" role="button">
-                            <i class="fab fa-facebook"></i>
-            &nbsp;Sign Up with Facebook
-          </a>
+                <div className="card social-block">
+                    <div className="card-body">
+                        <a className="btn btn-block btn-facebook" href="/auth/facebook" role="button">
+                            <i className="fab fa-facebook"></i>
+                            &nbsp;Sign Up with Facebook
+                        </a>
                     </div>
                 </div>
             </div>
