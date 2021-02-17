5447b66f6414a929c50916d98bd6c9426d6e1434
Sarthak <61661622+sg60692@users.noreply.github.com>
2021-02-17 18:50:25 +0530

    sarthak

--- client/src/Components/post/post.js
+++ client/src/Components/post/post.js
@@ -2,14 +2,15 @@ import React from 'react';
 import './post.css';
 function post(props) {
     return (
-        <div>
-            <div className="post">
-                <h2 className="post-h2">{props.heading}</h2>
-                <p className="post-poster">Posted by - {props.postedBy}</p>
+        <div className="post">
+            <div className="post-question">
+                <h2 className="post-question-heading">{props.heading}</h2>
+                <p className="post-question-user">Posted by - {props.postedBy}</p> 
+                <p className="post-question-user">Posted by - {props.postedBy}</p>
             </div>
             <br />
-            <div className="post post-answer">
-                <p className="post-answerer">Answered by -{props.answers[0].postedBy}</p> <br />
+            <div className="post-answer">
+                <p className="post-answere-user">Answered by -{props.answers[0].postedBy}</p> <br />
                 <p>{props.answers[0].description}</p>
             </div>
         </div>
